d24acd66c980d49c57f2ec68cde6de27036e4273
uday <udaya.marolix@gmail.com>
2023-03-24 11:50:12 +0530

    updated

---
+++ maven-first-project/src/main/java/com/uday/App.java
@@ -0,0 +1,13 @@
+package com.uday;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Uday your first maven project was completed now" );
+    }
+}
